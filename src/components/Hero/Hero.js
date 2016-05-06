2d2adfbc72ba83cf0b59962c552d566911074103
Robby Sahm <robertsahm@gmail.com>
2016-05-05 21:28:56 -0400

    added media queries

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,13 +4,18 @@ import { Link } from 'react-router'
 class Hero extends Component {
   render() {
     const styles = require('./Hero.scss')
+    const fireBig = require('./fire-1.svg')
 
     return (
       <div className={styles.hero}>
+
         <h1>Robert Sahm</h1>
         <h1>New York City</h1>
         <h1>Web Development</h1>
         <h1>Design</h1>
+
+        <div className={styles.fire}>
+        </div>
       </div>
     )
   }
