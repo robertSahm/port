d0356f359106ad901a48a968a82f705ba5a9e588
Robby Sahm <robertsahm@gmail.com>
2016-05-17 16:53:02 -0400

    home project content in place

--- src/components/JobList/JobList.js
+++ src/components/JobList/JobList.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 import styles from './JobList.scss'
 import FireBlue from '../FireBlue/FireBlue'
+import divider_line from './divider_line.png';
+import lucera_home from './lucera_ss_home.png';
+import alpine_home from './alpine_home.png';
+import carol_home from './carol_home.png';
 
 class JobList extends Component {
 
@@ -10,20 +14,60 @@ class JobList extends Component {
     return (
       <div className={styles.joblist_wrap}>
         <FireBlue />
+        <img src={divider_line} className={styles.divider_line}/>
         <h2 className={styles.joblist_header}>Selected Works</h2>
-        <div className={styles.dummy_text}>
-          <h2>
-            Lorem ipsum dolor sit amet, vim tollit qualisque signiferumque eu. Ut iudico integre usu. Id nusquam epicurei hendrerit vel, pri audire conceptam elaboraret ea. Vel et explicari conclusionemque, perpetua qualisque efficiendi has in, id augue iudico pro.
 
-            Nibh option rationibus no quo, inermis fastidii qui cu. Ne ullum ornatus eloquentiam quo, an qui possit cetero accommodare. His ne minimum neglegentur, pro amet erroribus percipitur ne, vis et verear liberavisse. Eam elitr epicuri insolens in, persecuti mediocritatem an vel.
+        <div className={styles.projects_wrap}>
+          <div className={styles.lucera_project_wrap}>
+            <div className={styles.project_home_img}>
+              <img src={lucera_home} />
+            </div>
+            <div className={styles.project_description}>
+              <h3>Lucera Financial Infrastructures</h3>
+              <h5>Front End Engineer</h5>
+              <ul>
+                <li><span>Design and code tools and UI's for traders and engineers</span></li>
+                <li><span>Create internal and public-facing style guides</span></li>
+                <li><span>Design and code branding and marketing materials, including main marketing site at Lucera.com</span></li>
+                <li><span>Raw JavaScript, React.js, Sass, D3.js, SmartOS</span></li>
+              </ul>
+            </div>
+          </div>
 
-            Omnis antiopam expetendis no eum, eum dico epicuri et. Probo persecuti suscipiantur cu pro, ei sit viderer appetere reprehendunt, per diam choro invidunt ne. Viderer ocurreret intellegat duo id, latine reprehendunt quo ei, has an luptatum periculis forensibus. No definiebas percipitur duo. Cum vero exerci definitiones ad. Ius cu solet nusquam assueverit.
+          <div className={styles.alpine_project_wrap}>
+            <div className={styles.project_home_img}>
+              <img src={alpine_home} />
+            </div>
+            <div className={styles.project_description}>
+              <h3>Alpine Labs</h3>
+              <h5>Front End Engineer</h5>
+              <ul>
+                <li><span>Design and code tools and UI's for traders and engineers</span></li>
+                <li><span>Create internal and public-facing style guides</span></li>
+                <li><span>Design and code branding and marketing materials, including main marketing site at Lucera.com</span></li>
+                <li><span>Raw JavaScript, React.js, Sass, D3.js, SmartOS</span></li>
+              </ul>
+            </div>
+          </div>
 
-            Ei quo latine denique, duo fabulas disputationi et, quem principes mediocritatem id nec. Ut mei libris quaeque, has putant tritani omittam ut. At reque noster bonorum vim. Vim dico debet delectus ne, ut quo exerci delectus instructior, an vim quem magna postea. Et has sumo lorem, discere denique no sed. Primis omnesque nominavi quo te, ad quem offendit eam.
+          <div className={styles.carol_project_wrap}>
+            <div className={styles.project_home_img}>
+              <img src={carol_home} />
+            </div>
+            <div className={styles.project_description}>
+              <h3>Carol Beehler Graphic Design</h3>
+              <h5>Front End Engineer</h5>
+              <ul>
+                <li><span>Design and code tools and UI's for traders and engineers</span></li>
+                <li><span>Create internal and public-facing style guides</span></li>
+                <li><span>Design and code branding and marketing materials, including main marketing site at Lucera.com</span></li>
+                <li><span>Raw JavaScript, React.js, Sass, D3.js, SmartOS</span></li>
+              </ul>
+            </div>
+          </div>
 
-            Ex mutat facilisis democritum sea, mei ei nisl hinc concludaturque. Nec ad audire nonumes, sit cetero lobortis consulatu ne. Sea an disputando contentiones, assum minim oratio ea pri. Reque nulla vitae et vim. Vis ex accusam platonem.
-          </h2>
         </div>
+
       </div>
     )
   }
